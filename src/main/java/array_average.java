2368231236740fb6e1a526cc4095721379220970
Shouzeb <shouzebhasan99@gmail.com>
2020-07-23 23:54:19 +0500

    changing

--- src/main/java/array_average.java
+++ src/main/java/array_average.java
@@ -5,13 +5,16 @@ public class array_average {
     public double arrayAverage(int[] arr){
 
         int total = 0;
-
+        double average=0.0;
+if(arr.length>0){
         for(int i=0; i<arr.length; i++){
             total = total + arr[i];
         }
-        double average = total / arr.length;
+        average= total / arr.length;
 
         return (average);
     }
-
+else {
+    return average;
 }
+}}
